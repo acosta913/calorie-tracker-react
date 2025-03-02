4634e9a8f411b661122cd0c26f4c39a97afaefc0
acosta913 <acostapaez913@gmail.com>
2025-03-02 01:15:49 -0500

    add reducer

---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,78 @@
+import { Activity } from "../types";
+
+export type ActivityActions =
+  | {
+      type: "save-activity";
+      payload: { newActivity: Activity };
+    }
+  | {
+      type: "set-activeId";
+      payload: { id: Activity["id"] };
+    }
+  | {
+      type: "delete-activity";
+      payload: { id: Activity["id"] };
+    }
+  | {
+      type: "restart-app";
+    };
+
+export type ActivityState = {
+  activities: Activity[];
+  activeId: Activity["id"];
+};
+
+const localStorageActivities = (): Activity[] => {
+  const activities = localStorage.getItem("activities");
+  return activities ? JSON.parse(activities) : [];
+};
+
+export const initialState: ActivityState = {
+  activities: localStorageActivities(),
+  activeId: "",
+};
+
+export const ActivityReducer = (
+  state: ActivityState = initialState,
+  action: ActivityActions
+) => {
+  if (action.type === "save-activity") {
+    let updatedAtivities: Activity[] = [];
+    if (state.activeId) {
+      updatedAtivities = state.activities.map((activity) =>
+        activity.id === state.activeId ? action.payload.newActivity : activity
+      );
+    } else {
+      updatedAtivities = [...state.activities, action.payload.newActivity];
+    }
+    return {
+      ...state,
+      activities: updatedAtivities,
+      activeId: "",
+    };
+  }
+
+  if (action.type === "set-activeId") {
+    return {
+      ...state,
+      activeId: action.payload.id,
+    };
+  }
+
+  if (action.type === "delete-activity") {
+    return {
+      ...state,
+      activities: state.activities.filter(
+        (activity) => activity.id !== action.payload.id
+      ),
+    };
+  }
+
+  if (action.type === "restart-app") {
+    return {
+      activities: [],
+      activeId: "",
+    };
+  }
+  return state;
+};
